40ee02259cd2dc650d471eaa8cd0aae731114ab4
tuagkcuf <jokersms222@gmail.com>
2023-03-04 12:32:25 +0300

    minor changes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 import { Server } from "socket.io"
+const io = new Server()
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
