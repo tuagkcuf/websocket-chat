fa14646ae9d77dcd61126435bb1c15a82d940d86
tuagkcuf <jokersms222@gmail.com>
2023-03-04 13:16:32 +0300

    setup of socket.io

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
-import { Server } from "socket.io"
-const io = new Server(app);
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
+io.on("connection", (socket) => {
+    socket.on("chat message", (msg) => {
+        io.emit('chat message', msg)
+    });
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('chat message', "user left the chat")
+    })
+});
 
-
-app.listen(PORT, () => console.log(`started on port: ${PORT}`));
+server.listen(PORT, () => console.log(`started on port: ${PORT}`));
