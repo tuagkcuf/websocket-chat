051dca4f726baee4a17ed6bd77fe09c400e73c29
tuagkcuf <jokersms222@gmail.com>
2023-03-04 12:32:15 +0300

    added socket.io paths

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import { Server } from "socket.io"
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello world</h1>')
-})
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+
 
 app.listen(PORT, () => console.log(`started on port: ${PORT}`));
